123642d456a8afbe455387a734ae9e13e99b20f5
santiago <santiago.ottolini97@hotmail.com>
2021-04-20 10:52:15 -0300

    HTML Structure

---
+++ src/Slider.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Slider() {
+  return (
+    <div className="sliderContainer">
+      <input type="range" className="sldier" />
+    </div>
+  );
+}
