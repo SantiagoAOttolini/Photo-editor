771488918471dce8e9757744404d08c8dfbe8262
santiago <santiago.ottolini97@hotmail.com>
2021-04-20 12:30:30 -0300

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Slider from "./Slider";
+import SlidebarItem from "./SlidebarItem";
 
 function App() {
   return (
     <div className="container">
-      <div className="mainImage">Image</div>
-      <div className="sidebar">Sidebar</div>
+      <div className="mainImage" />
+      <div className="sidebar">
+      <SlidebarItem/>
+      <SlidebarItem/>
+      </div>
       <Slider />
     </div>
   );
